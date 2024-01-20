da7249b47556c8984c68e5da490ca462ca4c7e15
Cristóbal Sarmiento Rodríguez <cristobal.sarmiento101@alu.ulpgc.es>
2024-01-20 15:34:54 +0000

    Class NextImageCommand created.

---
+++ src/main/java/software/ulpgc/image_viewer/NextImageCommand.java
@@ -0,0 +1,17 @@
+package software.ulpgc.image_viewer;
+
+public class NextImageCommand implements Command {
+
+    private final ImageDisplay imageDisplay;
+
+    public NextImageCommand(ImageDisplay imageDisplay) {
+        this.imageDisplay = imageDisplay;
+    }
+
+    @Override
+    public void execute() {
+
+        imageDisplay.show(imageDisplay.image().next());
+
+    }
+}
